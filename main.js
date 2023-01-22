3fb7c0afe97659e49ba76ff7614edab4d0491c75
Snorri967 <84525118+Snorri967@users.noreply.github.com>
2023-01-22 05:44:31 +0000

    Add display features

--- main.js
+++ main.js
@@ -15,13 +15,47 @@ function divide (n1, n2) {
 }
 
 function operate(operator, n1, n2) {
-  if (operator === add) {
+  if (operator === "+") {
     return add(n1, n2);
-  } else if (operator === subtract) {
+  } else if (operator === "-") {
     return subtract(n1, n2);
-  } else if (operator === multiply) {
+  } else if (operator === "*") {
     return multiply(n1, n2) 
-  } else if (operator === divide) {
+  } else if (operator === "/") {
     return divide(n1, n2);
   }
 }
+
+const displayValue = document.querySelector(".displayValue");
+const calculatorButtons = document.querySelectorAll("button");
+
+let num1 = 0;
+let num2 = 0;
+
+function getNumericValue() {
+  calculatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      displayValue.textContent += button.textContent;
+      if (clearCalculation()) {
+        displayValue.textContent = "";
+      } else if (numericMaxLength()) {
+        displayValue.textContent = displayValue.textContent.substring(0, 7);
+      } else {
+        return value = displayValue.textContent;
+      }
+    })
+  })
+}
+
+function clearCalculation() {
+  return displayValue.textContent.includes("CE");
+}
+
+function numericMaxLength() {
+  return displayValue.textContent.length > 7;
+}
+
+
+
+getNumericValue();
+
