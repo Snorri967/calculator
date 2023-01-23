5619cc1a9de3f7ac7cf39b3eabb2dc4c8b702d1f
Snorri967 <84525118+Snorri967@users.noreply.github.com>
2023-01-23 07:47:52 +0000

    Update Javascript to add 2 numbers together

--- main.js
+++ main.js
@@ -1,3 +1,72 @@
+const displayValue = document.querySelector(".displayValue");
+const operatorValue = document.querySelector(".operatorValue");
+const numbers = document.querySelectorAll(".number");
+const clearScreen = document.querySelector(".clearButton");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equalsButton");
+
+let firstValue = true;
+let secondValue = false;
+
+let counter = 0;
+
+let firstNum = 0;
+let secondNum = 0;
+
+let operatorInUse = "";
+
+displayValue.textContent = "";
+
+numbers.forEach(num => {
+  num.addEventListener("click", (event) => {
+    displayValue.textContent += event.target.textContent;
+    console.log(displayValue.textContent);
+    numericMaxLength();
+    if (firstValue) {
+      firstNum = Number(displayValue.textContent);
+    } else if (secondValue) {
+      secondNum = Number(displayValue.textContent);
+    }
+  })
+})
+
+operators.forEach(operator => {
+  operator.addEventListener("click", (event) => {
+    operatorValue.textContent = event.target.textContent;
+    operatorInUse = operatorValue.textContent;
+    switchOperand();
+  })
+})
+
+equals.addEventListener("click", (event) => {
+  displayValue.textContent = "";
+  operatorValue.textContent = event.target.textContent;
+  displayValue.textContent = operate(operatorInUse, firstNum, secondNum) 
+})
+
+clearScreen.addEventListener("click", () => {
+  location.reload();
+})
+
+function switchOperand() {
+  counter++;
+  if (counter === 1) {
+    firstValue = false;
+    secondValue = true;
+    return displayValue.textContent = "";
+  } else if (counter === 2) {
+    secondValue = false;
+    firstValue = true;
+    return displayValue.textContent = "";
+  }
+}
+
+function numericMaxLength() {
+  if (displayValue.textContent.length > 10) {
+    return displayValue.textContent = displayValue.textContent.substring(0, 10);
+  }
+}
+
 function add(n1, n2) {
   return n1 + n2;
 }
@@ -25,37 +94,3 @@ function operate(operator, n1, n2) {
     return divide(n1, n2);
   }
 }
-
-const displayValue = document.querySelector(".displayValue");
-const calculatorButtons = document.querySelectorAll("button");
-
-let num1 = 0;
-let num2 = 0;
-
-function getNumericValue() {
-  calculatorButtons.forEach(button => {
-    button.addEventListener("click", () => {
-      displayValue.textContent += button.textContent;
-      if (clearCalculation()) {
-        displayValue.textContent = "";
-      } else if (numericMaxLength()) {
-        displayValue.textContent = displayValue.textContent.substring(0, 7);
-      } else {
-        return value = displayValue.textContent;
-      }
-    })
-  })
-}
-
-function clearCalculation() {
-  return displayValue.textContent.includes("CE");
-}
-
-function numericMaxLength() {
-  return displayValue.textContent.length > 7;
-}
-
-
-
-getNumericValue();
-
