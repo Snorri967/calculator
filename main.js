c493f86f4d40570456138c74f98bbdf5444d9f8a
Snorri967 <84525118+Snorri967@users.noreply.github.com>
2023-02-07 23:21:23 +0000

    Redesign JS & add backspace button

--- main.js
+++ main.js
@@ -1,71 +1,139 @@
 const displayValue = document.querySelector(".displayValue");
 const operatorValue = document.querySelector(".operatorValue");
-const numbers = document.querySelectorAll(".number");
-const clearScreen = document.querySelector(".clearButton");
+const operands = document.querySelectorAll(".number");
+const clear = document.querySelector(".clearButton");
 const operators = document.querySelectorAll(".operator");
 const equals = document.querySelector(".equalsButton");
+const backspace = document.querySelector(".backspace");
 
-let firstValue = true;
-let secondValue = false;
+let firstOperand = 0;
+let secondOperand = 0;
 
-let counter = 0;
+let firstOperator = "";
+let secondOperator = "";
 
-let firstNum = 0;
-let secondNum = 0;
+let operandDisplay = "";
 
-let operatorInUse = "";
+let runningTotal = "";
 
-displayValue.textContent = "";
+displayValue.innerText = 0;
 
-numbers.forEach(num => {
-  num.addEventListener("click", (event) => {
-    displayValue.textContent += event.target.textContent;
-    console.log(displayValue.textContent);
-    numericMaxLength();
-    if (firstValue) {
-      firstNum = Number(displayValue.textContent);
-    } else if (secondValue) {
-      secondNum = Number(displayValue.textContent);
+function getOperands() {
+  operands.forEach(operand => {
+    operand.addEventListener("click", () => {
+      updateOperand(operand.innerText);
+      limitCharacterLength();
+      
+    })
+  })
+}
+
+getOperands();
+
+function getOperator() {
+  operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+      updateOperator(operator.innerText);
+    })
+  })
+}
+
+getOperator();
+
+function updateOperand(operand) {
+  if (firstOperand !== 0 && secondOperator !== "") {
+    operandDisplay += operand;
+    secondOperand = operandDisplay;
+    firstOperator = operatorValue.innerText;
+    displayValue.innerText = operandDisplay;
+  } else if (firstOperator === "") {
+    operandDisplay += operand;
+    displayValue.innerText = operandDisplay;
+    firstOperand = operandDisplay;
+  } else if (firstOperand !== 0 && firstOperator !== "") {
+    operandDisplay += operand;
+    displayValue.innerText = operandDisplay;
+    secondOperand = operandDisplay;
+  } 
+}
+
+function updateOperator(operator) {
+  if (firstOperand === 0 && firstOperator === "") {
+    operatorValue.innerText = "";
+  } else if (firstOperand !== 0 && firstOperator === "") {
+    firstOperator = operator;
+    operatorValue.innerText = firstOperator;
+    operandDisplay = "";
+  } else if (firstOperand !== 0 && secondOperand !== 0) {
+    secondOperator = operator;
+    total = operate(firstOperator, Number(firstOperand), Number(secondOperand));
+    if (firstOperator === "/") {
+      runningTotal = parseFloat(total).toFixed(2);
+    } else {
+      runningTotal = total;
+    }
+    firstOperand = runningTotal 
+    displayValue.innerText = firstOperand;
+    operatorValue.innerText = secondOperator;
+    secondOperand = 0;
+    firstOperator = "";
+    operandDisplay = "";
+  }
+}
+
+function operateEquals() {
+  equals.addEventListener("click", () => {
+    let total = "";
+    if (firstOperand === 0) {
+      displayValue.innerText = "Error";
+    } else if (firstOperand !== 0 && secondOperand === 0) {
+      displayValue.innerText = firstOperand;
+    } else if (firstOperand !== 0 && secondOperand !== 0) {
+      total = operate(firstOperator, Number(firstOperand), Number(secondOperand));
+      console.log(firstOperator);
+      if (firstOperator === "/") {
+        runningTotal = parseFloat(total).toFixed(2);
+        displayValue.innerText = runningTotal
+      } else {
+        runningTotal = total;
+      }
+      displayValue.innerText = runningTotal;
+      operatorValue.innerText = equals.innerText;
     }
   })
-})
+}
 
-operators.forEach(operator => {
-  operator.addEventListener("click", (event) => {
-    operatorValue.textContent = event.target.textContent;
-    operatorInUse = operatorValue.textContent;
-    switchOperand();
+operateEquals();
+
+function clearCalculator() {
+  clear.addEventListener("click", () => {
+    firstOperand = 0;
+    secondOperand = 0;
+    firstOperator = "";
+    secondOperator = "";
+    operandDisplay = "";
+    runningTotal = "";
+    displayValue.innerText = 0;
+    operatorValue.innerText = "";
   })
-})
+}
 
-equals.addEventListener("click", (event) => {
-  displayValue.textContent = "";
-  operatorValue.textContent = event.target.textContent;
-  displayValue.textContent = operate(operatorInUse, firstNum, secondNum) 
-})
+clearCalculator();
 
-clearScreen.addEventListener("click", () => {
-  location.reload();
-})
-
-function switchOperand() {
-  counter++;
-  if (counter === 1) {
-    firstValue = false;
-    secondValue = true;
-    return displayValue.textContent = "";
-  } else if (counter === 2) {
-    secondValue = false;
-    firstValue = true;
-    return displayValue.textContent = "";
+function limitCharacterLength() {
+  if (operandDisplay.length >= 7) {
+    displayValue.innerText = operandDisplay.substring(0, 7);
   }
 }
 
-function numericMaxLength() {
-  if (displayValue.textContent.length > 10) {
-    return displayValue.textContent = displayValue.textContent.substring(0, 10);
-  }
+function eraseNumber() {
+  backspace.addEventListener("click", () => {
+    operandDisplay = operandDisplay.substring(0, operandDisplay.length - 1);
+    displayValue.innerText = operandDisplay;
+  })
 }
+
+eraseNumber();
 
 function add(n1, n2) {
   return n1 + n2;
